031ccaf8f834fc8643d3ce3dc07d582052029bf2
lakshmisr21 <sr.lakshmi2003@gmail.com>
2021-10-29 15:30:48 +0530

    Correction

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
+//if (process.env.NODE_ENV !== 'production') {
+   // require('dotenv').config()
+ // }
 
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -44,7 +45,8 @@ if(process.env.NODE_ENV === "production"){
 
 
   const mongoose = require('mongoose')
-  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true,useUnifiedTopology: true})
+  let uri = process.env.DATABASE_URL
+  mongoose.connect(uri, { useNewUrlParser: true,useUnifiedTopology: true})
   const db = mongoose.connection
   db.on('error', error => console.error(error))
   db.once('open', () => console.log('Connected to Mongoose Database'))
